284a6203a545feff4bc651fc2705d8ae5b69b7ca
Andrei <97633022+Zmei55@users.noreply.github.com>
2023-05-11 18:24:27 +0200

    Data's added

---
+++ src/main/java/data/PatientData.java
@@ -0,0 +1,7 @@
+package data;
+
+public class PatientData {
+    public static final String PATIENT_NAME = "Sofia Wagner";
+    public static final String PATIENT_BIRTH_DAY = "1991-04-27T00:00:00.000Z";
+    public static final String PATIENT_CARD_NUMBER = "446640502";
+}
---
+++ src/main/java/data/UserData.java
@@ -0,0 +1,7 @@
+package data;
+
+public class UserData {
+    public static final String USER_NAME="qwe";
+    public static final String USER_PASSWORD="qwe123";
+    public static final String USER_STATION="chirurgisch";
+}
