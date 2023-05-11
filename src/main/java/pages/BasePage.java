3d800afb1cb49b86cfe7134e63a364ff4aa9dc79
Andrei <97633022+Zmei55@users.noreply.github.com>
2023-05-11 18:28:13 +0200

    Base's added

---
+++ src/main/java/pages/BasePage.java
@@ -0,0 +1,65 @@
+package pages;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BasePage {
+    protected WebDriver driver;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this); // строит все страницы (new) / отвечает за инициализации страниц и эл-ов
+    }
+
+    public void click(WebElement element) {
+        element.click();
+    }
+
+    public void type(WebElement element, String text) {
+        if(text != null) {
+            click(element);
+            element.clear();
+            element.sendKeys(text);
+        }
+    }
+
+    public boolean shouldHaveText(WebElement element, String text, int time) {
+        return new WebDriverWait(driver, Duration.ofSeconds(time)).until(ExpectedConditions.textToBePresentInElement(element, text));
+        // ожидает ...секуд, потом проверяет наличие эл-та
+    }
+
+    // скрол
+    public void clickWithJSExecutor(WebElement element, int x, int y) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy("+ x +", "+ y +")");
+        element.click();
+    }
+
+    public void typeWithJSExecutor(WebElement element, String text, int x, int y) {
+        if(text != null) {
+            clickWithJSExecutor(element, x, y);
+            element.clear();
+            element.sendKeys(text);
+        }
+    }
+
+    public void pause(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // скрывает рекламу по id
+    public void hideAd() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("document.getElementById('...').style.dispay='none';");
+    }
+}
